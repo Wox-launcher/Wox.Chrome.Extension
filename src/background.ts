186e8064f59ad36804edd333650ebd193213fa1c
qianlifeng <qianlf2010@gmail.com>
2024-05-22 15:50:38 +0800

    Refactor extension for Wox integration
    The commit involves a major overhaul of the existing chrome extension in order to ensure compatibility with Wox. This included deletion of several files, creation of new ones, and modifications of others. The purpose is to ensure seamless interaction between the extension and Wox as part of the broader initiative to improve overall user experience.

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,110 @@
-function polling() {
-  // console.log("polling");
-  setTimeout(polling, 1000 * 30);
+import {DEFAULT_SERVER_PORT} from "./const";
+
+let ws: WebSocket | null = null;
+let lastPongTime = 0;
+
+export interface Message {
+    method: string;
+    data: string;
 }
 
-polling();
+export interface OpenTabData {
+    tabId: number;
+    tabIndex: number;
+    windowId: number;
+}
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+    if (msg.method === "isConnected") {
+        let isConnected = new Date().getTime() - lastPongTime < 3000;
+        sendResponse(isConnected);
+    }
+});
+
+// reconnect to the server when the port is changed
+chrome.storage.onChanged.addListener((changes, namespace) => {
+    if (namespace === "sync" && changes.port) {
+        if (ws) {
+            ws.close();
+        }
+        console.log("port changed, reconnecting to the server");
+        connect();
+    }
+});
+
+// send ping message to server every 1 second
+setInterval(() => {
+    sendMsg({
+        method: "ping",
+        data: new Date().toLocaleTimeString(),
+    })
+}, 1000);
+
+// send opened tabs to the server every 1 second
+setInterval(() => {
+    chrome.tabs.query({}, (tabs) => {
+        sendMsg({
+            method: "tabs",
+            data: JSON.stringify(tabs.map((tab) => {
+                return {
+                    tabId: tab.id || -1,
+                    windowId: tab.windowId || -1,
+                    tabIndex: tab.index || -1,
+                    title: tab.title || "",
+                    url: tab.url || "",
+                    pinned: tab.pinned || false,
+                    highlighted: tab.highlighted || false,
+                };
+            })),
+        });
+    });
+}, 1000);
+
+function connect() {
+    chrome.storage.sync.get(
+        {
+            port: DEFAULT_SERVER_PORT,
+        },
+        (items) => {
+            const port = items.port;
+            console.log("connecting to the server, port:", port);
+
+            ws = new WebSocket("ws://localhost:" + port + "/ws");
+            ws.onopen = () => {
+                console.log("connected to the server");
+            };
+            ws.onmessage = (event) => {
+                const msg = JSON.parse(event.data) as Message;
+                console.log("message from server:", msg);
+                if (msg.method === "pong") {
+                    lastPongTime = new Date().getTime();
+                }
+                if (msg.method === "highlightTab") {
+                    const tabData = JSON.parse(msg.data) as OpenTabData;
+                    highlightTab(tabData);
+                }
+            };
+            ws.onclose = () => {
+                console.log("disconnected from the server");
+                // reconnect
+                setTimeout(() => {
+                    connect();
+                }, 3000);
+            };
+        }
+    );
+}
+
+function highlightTab(tab: OpenTabData) {
+    chrome.tabs.highlight({tabs: [tab.tabIndex], windowId: tab.windowId}, () => {
+        console.log("highlighted tab:", tab);
+    });
+}
+
+function sendMsg(msg: Message) {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+        ws.send(JSON.stringify(msg));
+    }
+}
+
+connect();
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,58 +1,74 @@
-import React, { useEffect, useState } from "react";
-import { createRoot } from "react-dom/client";
+import React, {useEffect, useState} from "react";
+import {createRoot} from "react-dom/client";
+import {DEFAULT_SERVER_PORT} from "./const";
 
 const Popup = () => {
-  const [count, setCount] = useState(0);
-  const [currentURL, setCurrentURL] = useState<string>();
+    const [port, setPort] = useState<string>("34988");
+    const [status, setStatus] = useState<string>("");
+    const [isConnected, setIsConnected] = useState<boolean>(false);
 
-  useEffect(() => {
-    chrome.action.setBadgeText({ text: count.toString() });
-  }, [count]);
-
-  useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      setCurrentURL(tabs[0].url);
-    });
-  }, []);
-
-  const changeBackground = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      const tab = tabs[0];
-      if (tab.id) {
-        chrome.tabs.sendMessage(
-          tab.id,
-          {
-            color: "#555555",
-          },
-          (msg) => {
-            console.log("result message:", msg);
-          }
+    useEffect(() => {
+        chrome.storage.sync.get(
+            {
+                port: DEFAULT_SERVER_PORT,
+            },
+            (items) => {
+                setPort(items.port);
+            }
         );
-      }
-    });
-  };
 
-  return (
-    <>
-      <ul style={{ minWidth: "700px" }}>
-        <li>Current URL: {currentURL}</li>
-        <li>Current Time: {new Date().toLocaleTimeString()}</li>
-      </ul>
-      <button
-        onClick={() => setCount(count + 1)}
-        style={{ marginRight: "5px" }}
-      >
-        count up
-      </button>
-      <button onClick={changeBackground}>change background</button>
-    </>
-  );
+        checkConnected();
+    }, []);
+
+    useEffect(() => {
+        const t = setInterval(checkConnected, 1000);
+        return () => clearInterval(t);
+    }, []);
+
+    const checkConnected = () => {
+        chrome.runtime.sendMessage(
+            {
+                method: "isConnected",
+                data: "",
+            },
+            (response) => {
+                console.log("isConnected:", response);
+                setIsConnected(response);
+            }
+        );
+    };
+
+    const saveOptions = () => {
+        chrome.storage.sync.set(
+            {
+                port: port,
+            },
+            () => {
+                setStatus("Options saved.");
+                const id = setTimeout(() => {
+                    setStatus("");
+                }, 1000);
+                return () => clearTimeout(id);
+            }
+        );
+    };
+
+    return (
+        <div style={{width: 200}}>
+            <div style={{marginBottom: 20}}>
+                Server Port: <input type="text" value={port} onChange={(event) => setPort(event.target.value)}/>
+            </div>
+            <div style={{marginBottom: 20}}>Connected: {isConnected ? "yes" : "no"}</div>
+            <button onClick={saveOptions}>Save</button>
+            <div>{status}</div>
+        </div>
+    );
 };
 
 const root = createRoot(document.getElementById("root")!);
 
 root.render(
-  <React.StrictMode>
-    <Popup />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Popup/>
+    </React.StrictMode>
 );
